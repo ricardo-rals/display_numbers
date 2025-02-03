8868b3a15a96faf018bd6f004345743e5272dd7d
Ricardo Augusto <ricardo.rals.dev@gmail.com>
2025-02-02 21:40:06 -0300

    feat: add main LED blinking functionality

---
+++ includes/led_control.h
@@ -0,0 +1,10 @@
+#ifndef LED_CONTROL_H
+#define LED_CONTROL_H
+
+#include "pico/stdlib.h"
+
+#define RED_LED 13
+
+void blink_red_led();
+
+#endif // LED_CONTROL_H
---
+++ src/led_control.c
@@ -0,0 +1,16 @@
+#include "../includes/led_control.h"
+
+void blink_red_led() {
+    static uint32_t last_toggle_time = 0;
+    uint32_t current_time = to_us_since_boot(get_absolute_time());
+
+    // Blink every 200ms (5 times per second)
+    if (current_time - last_toggle_time > 200000) {
+        last_toggle_time = current_time;
+
+        // Toggle the LED state
+        static bool led_on = false;
+        led_on = !led_on;
+        gpio_put(RED_LED, led_on ? 1 : 0);
+    }
+}
